f84bdc3e5ac19a0de1e824b7ff3c1106f5b8e9b8
Jhair14 <129466823+Jhair14@users.noreply.github.com>
2025-08-18 00:14:48 -0400

    v5-back

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(helmet());
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3001','http://localhost:5173','https://formulario-bomberos.vercel.app/'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3001','http://localhost:5173','https://formulario-bomberos.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
